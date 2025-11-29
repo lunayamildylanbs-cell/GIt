95c07f92ca23487557371657bea7e7ea74f96042
escoripiozzz-alt <escoripiozzz@gmail.com>
2025-11-29 08:11:23 -0400

    mensaje de cambios

---
+++ VC.js
@@ -0,0 +1,6 @@
+console.log("Bienvenido a mi portafolio");
+alert("Gracias por visitar mi pagina");
+//alert("Gracias por visitar mi pagina");
+document.querySelector('h1').addEventListener('clck',()=>{
+    alert('Gracias por visitar mi pagina');
+});
